a4f3fb5aff6e7caa0e70d0321c2f59eab53f75d8
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-12-10 21:38:09 +0100

    uche

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,17 +1,17 @@
 import AboutUs from "../Components/AboutUs/About";
+import BusinessSolutions from "../Components/BusinessSolution/BusinessSolution";
 import CaseStudies from "../Components/CaseStudies/CaseStudies";
 import GetStarted from "../Components/GetStarted/GetStarted";
 
 function Home() {
-    return (
-      <div >
-<CaseStudies/>
-        <AboutUs/>
-        <GetStarted/>
-        
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+  return (
+    <div>
+      <CaseStudies />
+      <BusinessSolutions />
+      <AboutUs />
+      <GetStarted />
+    </div>
+  );
+}
+
+export default Home;
