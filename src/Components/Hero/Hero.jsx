d204dd5a59b46eece9c5ff7173a61749c75c54f4
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-12-11 16:01:35 +0100

    new

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Bg from "../../Assets/Images/herobg.png";
+import Bg from "../../Assets/Images/herohesa.png";
 import phone from "../../Assets/Icons/phone.png";
 const Hero = () => {
   return (
-    <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: `url(${Bg})` }}>
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-r from-blue-900 to-purple-900 opacity-60"></div>
+    <div className="relative h-screen bg-center bg-cover" style={{ backgroundImage: `url(${Bg})`,
+    backgroundPosition: "center 20%" }}>
+ 
 
       {/* Content */}
       <div className="relative z-10 flex flex-col justify-center items-start h-full px-4 mx-auto lg:w-[90%] lg:px-8 text-white">
-      <div className="flex items-center justify-center lg:justify-start mb-6">
+      <div className="flex items-center justify-center mb-6 lg:justify-start">
           <span className="text-[12.7px] font-semibold font-sanss leading-4 tracking-wider uppercase">
             Unlock Your Business Potential
           </span>
@@ -28,12 +28,12 @@ const Hero = () => {
         </p>
 
         {/* Buttons */}
-        <div className="flex flex-col font-sans sm:flex-row sm:items-center space-y-4 sm:space-y-0 sm:space-x-6">
-          <button className="bg-red-500 hover:bg-red-600 text-white px-8 py-3  text-sm lg:text-base shadow-md">
+        <div className="flex flex-col space-y-4 font-sans sm:flex-row sm:items-center sm:space-y-0 sm:space-x-6">
+          <button className="px-8 py-3 text-sm text-white bg-red-500 shadow-md hover:bg-red-600 lg:text-base">
             Learn More →
           </button>
-          <a href="tel:+2348034474677" className="flex items-center text-sm lg:text-base font-medium">
-            <span className="  font-sans p-2 mr-1 shadow-md"><img src={phone} alt="Nav logo" className="w-full h-full  " /></span>
+          <a href="tel:+2348034474677" className="flex items-center text-sm font-medium lg:text-base">
+            <span className="p-2 mr-1 font-sans shadow-md "><img src={phone} alt="Nav logo" className="w-full h-full " /></span>
             +2348034474677
           </a>
         </div>
